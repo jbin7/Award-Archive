175233f659596684cd0e243c07a86f8e58b0daa1
jbin <kjb7907@gmail.com>
2023-01-12 03:52:11 +0900

    project init

---
+++ src/routes/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { page } from '$app/stores';	
+</script>
+
+<header>
+	header
+	<nav>
+	</nav>
+</header>
+
+<style>
+
+</style>
---
+++ src/routes/test/[slug]/+page.svelte
@@ -0,0 +1,16 @@
+<script>	
+	import { page } from '$app/stores';	
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	page test
+</section>
+
+<style>
+
+</style>
